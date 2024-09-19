fefcf52e25e0583a9b112db67243f2295a47a2f7
olearczuk <olearczuk.szymon@gmail.com>
2024-09-19 22:44:04 +0200

    OneshotChannel

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,3 @@
+mod oneshot_channel;
 mod spin_lock;
+
---
+++ src/oneshot_channel.rs
@@ -0,0 +1,94 @@
+use std::{
+    cell::UnsafeCell,
+    marker::PhantomData,
+    mem::MaybeUninit,
+    sync::atomic::{
+        AtomicBool,
+        Ordering::{Acquire, Release},
+    },
+    thread::{self, Thread},
+};
+
+pub struct OneshotChannel<T> {
+    message: UnsafeCell<MaybeUninit<T>>,
+    ready: AtomicBool,
+}
+
+unsafe impl<T> Sync for OneshotChannel<T> where T: Send {}
+
+pub struct Sender<'a, T> {
+    channel: &'a OneshotChannel<T>,
+    receiving_thread: Thread,
+}
+
+pub struct Receiver<'a, T> {
+    channel: &'a OneshotChannel<T>,
+    /// No Send because how thread parking is implemented
+    _no_send: PhantomData<*const ()>,
+}
+
+impl<T> OneshotChannel<T> {
+    pub const fn new() -> Self {
+        Self {
+            message: UnsafeCell::new(MaybeUninit::uninit()),
+            ready: AtomicBool::new(false),
+        }
+    }
+
+    pub fn split(&mut self) -> (Sender<T>, Receiver<T>) {
+        // In case of channel being reused after Sender and Receiving being dropped
+        *self = Self::new();
+        (
+            Sender {
+                channel: self,
+                receiving_thread: thread::current(),
+            },
+            Receiver {
+                channel: self,
+                _no_send: PhantomData,
+            },
+        )
+    }
+}
+
+impl<T> Sender<'_, T> {
+    pub fn send(self, message: T) {
+        unsafe { (*self.channel.message.get()).write(message) };
+        self.channel.ready.store(true, Release)
+    }
+}
+
+impl<T> Receiver<'_, T> {
+    pub fn receive(self) -> T {
+        while !self.channel.ready.swap(false, Acquire) {
+            thread::park();
+        }
+        unsafe { (*self.channel.message.get()).assume_init_read() }
+    }
+}
+
+impl<T> Drop for OneshotChannel<T> {
+    fn drop(&mut self) {
+        if *self.ready.get_mut() {
+            unsafe { (*self.message.get()).assume_init_drop() }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::OneshotChannel;
+    use std::thread;
+
+    #[test]
+    fn test() {
+        let mut channel = OneshotChannel::new();
+        thread::scope(|s| {
+            let (sender, receiver) = channel.split();
+            s.spawn(move || {
+                sender.send("test");
+            });
+            assert_eq!(receiver.receive(), "test");
+        })
+    }
+}
